1f8bf102029beb1aeb7d3e1deedf91de20bbd17e
Bryan Zúñiga <bryan.zuniga@udla.edu.ec>
2020-04-09 20:19:04 -0500

    adding text to a new item

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,12 @@ class TodoList extends StatefulWidget {
 class TodoListState extends State<TodoList> {
   List<String> _todoItems = [];
 
-  // This will be called each time the + button is pressed
-  void _addTodoItem() {
-    // Putting our code inside "setState" tells the app that our state has changed, and
-    // it will automatically re-render the list
-    setState(() {
-      int index = _todoItems.length;
-      _todoItems.add('Item ' + index.toString());
-    });
+// Instead of autogenerating a to_do item, _addTodoItem now accepts a string
+  void _addTodoItem(String task) {
+    // Only add the task if the user actually entered something
+    if (task.length > 0) {
+      setState(() => _todoItems.add(task));
+    }
   }
 
   // Build the whole list of to_do items
@@ -50,15 +48,36 @@ class TodoListState extends State<TodoList> {
     return new ListTile(title: new Text(todoText));
   }
 
-  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text('ToDo List')),
+      appBar: new AppBar(title: new Text('Todo List')),
       body: _buildTodoList(),
       floatingActionButton: new FloatingActionButton(
-          onPressed: _addTodoItem,
+          onPressed: _pushAddTodoScreen,
+          // pressing this button now opens the new screen
           tooltip: 'Add task',
           child: new Icon(Icons.add)),
     );
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+        // MaterialPageRoute will automatically animate the screen entry, as well
+        // as adding a back button to close it
+        new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+          appBar: new AppBar(title: new Text('Add a new task')),
+          body: new TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              _addTodoItem(val);
+              Navigator.pop(context); // Close the add to_do screen
+            },
+            decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)),
+          ));
+    }));
   }
 }
